585cd0e70cb566ed2c0849e24d5efa729b860dc4
Євгеній Шабля <vdfvfvsvx@gmail.com>
2023-08-28 19:45:06 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/Pages/DataViewModel.cs
@@ -0,0 +1,12 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+using Notes.Models;
+using System.Windows.Media;
+using Wpf.Ui.Controls;
+
+namespace Notes.ViewModels.Pages
+{
+    public partial class DataViewModel : ObservableObject
+    {
+       
+    }
+}
---
+++ ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using Wpf.Ui.Common;
+using Wpf.Ui.Controls;
+
+namespace Notes.ViewModels.Windows
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _applicationTitle = "Notes";
+
+        [ObservableProperty]
+        private ObservableCollection<object> _menuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "Home",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Home12 },
+                TargetPageType = typeof(Views.Pages.DashboardPage)
+            },
+            new NavigationViewItem()
+            {
+                Content = "Work",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.BuildingDesktop24 },
+                TargetPageType = typeof(Views.Pages.DataPage)
+            },
+             new NavigationViewItem()
+            {
+                Content = "Family",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.PeopleCommunity24 },
+                TargetPageType = typeof(Views.Pages.FamilyPage)
+            },
+              new NavigationViewItem()
+            {
+                Content = "For me",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.ShieldLock28},
+                TargetPageType = typeof(Views.Pages.ForMePage)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<object> _footerMenuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "Settings",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
+                TargetPageType = typeof(Views.Pages.SettingsPage)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<MenuItem> _trayMenuItems = new()
+        {
+            new MenuItem { Header = "Home", Tag = "tray_home" }
+        };
+    }
+}
